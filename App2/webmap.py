fe2a61a20830b78cd3234df76d8aba74cc9549b9
Stronach <alex@highlandercom.com>
2017-12-18 18:59:23 -0500

    update to webmap application, got it to map points on the map, still need to fix location settings

--- App2/webmap.py
+++ App2/webmap.py
@@ -2,26 +2,35 @@
 
 import folium
 import pandas
+import geopy
 from geopy.geocoders import Nominatim
 nom=Nominatim()
 
-data=pandas.read_csv("listings.csv")
-data["Address"] = data["ADDRESS"]+", "+ data["STATE"]+", "+ str(data["MAILING ZIPCODE"])
+data=pandas.read_csv("listings.csv", dtype={'MAILING ZIPCODE': 'str'})
+data["Address"] = data["ADDRESS"]+", "+ data["MAILING ZIPCODE"] +", "+ data["STATE"]
 
-for n in data["Address"]:
-
-    x=nom.geocode(data["Address"])
-    x.latitude
-    x.longitude
-
-lat = list(data["LAT"])
-lon = list(data["LON"])
-map = folium.Map(location =[43,-75], zoom_start=5, tiles="Mapbox Bright")
+data["Cordinates"]=data["Address"].apply(nom.geocode)
+n=data.Cordinates
+listing=str(data["TYPE"])
+print(listing)
 fg = folium.FeatureGroup(name="My Map")
+print(n)
+map = folium.Map(location =[43,-75], zoom_start=7)
+for lt in n:
+    data["LAT"]=lt.latitude
+    data["LON"]=lt.longitude
+    lat = data["LAT"]
+    lon = data["LON"]
+    for lt, ln in zip(lat,lon):
+        fg.add_child(folium.Marker(location = [lt,ln], popup="""listing"""+str(lt)+str(ln), icon=folium.Icon('green')))
+
+#lat = list(data["LAT"])
+#lon = list(data["LON"])
+# map = folium.Map(location =[43,-75], zoom_start=10)
+#fg = folium.FeatureGroup(name="My Map")
 
 
-for lt, ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location = [lt,ln], popup="""<h3>This is a popup<h3>""", icon=folium.Icon('green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
