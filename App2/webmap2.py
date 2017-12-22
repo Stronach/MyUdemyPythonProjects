25606c5ae70c02840351d33a2e8c5988f176e285
Stronach <alex@highlandercom.com>
2017-12-22 09:22:05 -0500

    Updates

--- App2/webmap2.py
+++ App2/webmap2.py
@@ -5,29 +5,35 @@ nom=Nominatim()
 
 def eval_results(x):
     try:
-        return (x.latitude, x.longitude)
+        data["LAT"]=x.latitude
+        data["LON"]=x.longitude
+        return x.latitude, x.longitude
     except:
-        return (None, None)
+        #data["Address"] = data["ADDRESS"]+", "+ data["MAILING ZIPCODE"] +", "+ data["STATE"]
+        #data["Cordinates"]=data["Address"].apply(nom.geocode).apply(lambda x: eval_results(x))
+        return (40, -75)
 
 data=pandas.read_csv("listings.csv", dtype={'MAILING ZIPCODE': 'str'})
 data["Address"] = data["ADDRESS"]+", "+ data["MAILING ZIPCODE"]
 
 data["Cordinates"]=data["Address"].apply(nom.geocode).apply(lambda x: eval_results(x))
 n=data.Cordinates
+lat=list(data["LAT"])
+lon=list(data["LON"])
 
-
-listing=str(data["TYPE"])
+listing=list(data["TYPE"])
 print(listing)
 fg = folium.FeatureGroup(name="My Map")
 print(n)
 map = folium.Map(location =[43,-75], zoom_start=7)
-for lt in n:
-    if data.Cordinates is None:
-        data["Address"] = data["ADDRESS"]+", "+ data["MAILING ZIPCODE"] +", "+ data["STATE"]
-        data["Cordinates"]=data["Address"].apply(nom.geocode).apply(lambda x: eval_results(x))
-        #print(data["LAT"])
-    for lt in zip(n):
-        fg.add_child(folium.Marker(location = n, popup="""listing """+str(listing), icon=folium.Icon('green')))
+#for cor in data.Cordinates:
+#    if cor is "(None, None)":
+#        data["Address"] = data["ADDRESS"]+", "+ data["MAILING ZIPCODE"] +", "+ data["STATE"]
+#        data["Cordinates"]=data["Address"].apply(nom.geocode).apply(lambda x: eval_results(x))
+#    print(data["Cordinates"])
+
+for lt, ln, lis in zip(lat,lon,listing):
+    fg.add_child(folium.Marker(location = [lt,ln], popup=lis, icon=folium.Icon(color='green')))
 
 #lat = list(data["LAT"])
 #lon = list(data["LON"])
