c1606a6adc431783c1d3ff6d8e7a59315dd7b7fe
Stronach <alex@highlandercom.com>
2017-12-29 20:18:12 -0500

    Finished website block app

--- blocker/blocker.py
+++ blocker/blocker.py
@@ -3,15 +3,33 @@
 import time
 from datetime import datetime as dt
 
+
 n = dt.now().year,dt.now().month,dt.now().day
 host = "/etc/hosts"
+hosttemp="hosts"
 redirect="127.0.0.1"
-whitelist=["facebook.com","www.facebook.com","wibx950.com", "www.wibx950.com"]
+weblist=["facebook.com","www.facebook.com","wibx950.com", "www.wibx950.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day, 8) < dt.now() <dt(dt.now().year,dt.now().month,dt.now().day,16):
-        print("working hours")
-
+        print("Working Hours")
+        with open(host,'r+') as file:
+            content = file.read()
+            #print(content)
+            for website in weblist:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" " + website + "\n")
     else:
         print("Fun Hours")
+        with open(host, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in weblist):
+                    file.write(line)
+            file.truncate()
+            #print(content)
+
     time.sleep(5)
